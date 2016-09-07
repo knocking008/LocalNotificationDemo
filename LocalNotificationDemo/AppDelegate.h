70e6af9243457f088913c19a2a2bb116ebbc1be9
杨植生 <vincent.yang@jinfuzi.com>
2016-09-07 09:36:28 +0800

    Initial Commit

---
+++ LocalNotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalNotificationDemo
+//
+//  Created by vincent on 16/9/7.
+//  Copyright © 2016年 FuTouBang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalNotificationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalNotificationDemo
+//
+//  Created by vincent on 16/9/7.
+//  Copyright © 2016年 FuTouBang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
